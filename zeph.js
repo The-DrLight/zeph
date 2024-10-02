006fa2aae7cb89c0221aca738cac603e267c11ea
Dr Light <167034291+The-DrLight@users.noreply.github.com>
2024-10-02 15:11:29 +0100

    Add files via upload

--- zeph.js
+++ zeph.js
@@ -5,23 +5,26 @@ document.getElementById("subscriptionForm").addEventListener("submit", function(
     const responseMessage = document.getElementById("responseMessage");
 
     if (validateEmail(email)) {
-        emailjs.send("service_xobv2nh", "template_eufmi7q", {
+        const templateParams = {
             user_email: email,
-        })
-        .then(function(response) {
-            responseMessage.textContent = "Thank you for subscribing!";
-            responseMessage.style.color = "green";
-            document.getElementById("subscriptionForm").reset();
-        }, function(error) {
-            responseMessage.textContent = "Oops! Something went wrong. Please try again.";
-            responseMessage.style.color = "red";
-        });
-
+        };
+    
+        emailjs.send('service_xobv2nh', 'template_eufmi7q', templateParams).then(
+            (response) => {
+                responseMessage.textContent = "Thank you for subscribing!";
+                responseMessage.style.color = "green";
+                document.getElementById("subscriptionForm").reset();
+            },
+            (error) => {
+                responseMessage.textContent = "Oops! Something went wrong. Please try again.";
+                responseMessage.style.color = "red";
+            }
+        );
+    
     } else {
         responseMessage.textContent = "Please enter a valid email address.";
         responseMessage.style.color = "red";
-    }
-});
+    }    
 
 
 function validateEmail(email) {
